bb468beffb666058538f4c86e898497afa4837c4
brianhouse <bjrian.house@gmail.com>
2019-03-04 13:06:23 -0500

    stubbed react app

---
+++ viewer/src/electron-starter.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+
+let main
+
+function createWindow () {
+    main = new BrowserWindow({ width: 1024, height: 800, resizable: false, show: false })
+    main.loadURL('http://localhost:3000')
+    main.once('ready-to-show', () => {
+      main.show()
+    })
+    main.on('closed', () => {
+        main = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (main === null) {
+        createWindow()
+    }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
