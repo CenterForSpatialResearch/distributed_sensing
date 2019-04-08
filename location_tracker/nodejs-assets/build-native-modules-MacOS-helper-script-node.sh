65452786e972a3d5aa08d6eec85d637d7a0cb327
Dimitri Leggas <dimitrileggas@dyn-160-39-8-65.dyn.columbia.edu>
2019-04-08 18:00:26 -0400

    hypercore running on dim machine

--- location_tracker/nodejs-assets/build-native-modules-MacOS-helper-script-node.sh
+++ location_tracker/nodejs-assets/build-native-modules-MacOS-helper-script-node.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
       # Helper script for Gradle to call node on macOS in case it is not found
-      export PATH=$PATH:/usr/local/lib/node_modules/npm/node_modules/npm-lifecycle/node-gyp-bin:/Users/jeevanfarias/Downloads/distributed_sensing/location_tracker/node_modules/nodejs-mobile-react-native/node_modules/.bin:/Users/jeevanfarias/Downloads/distributed_sensing/location_tracker/node_modules/.bin:/Users/jeevanfarias/.opam/default/bin:/Users/jeevanfarias/.rvm/gems/ruby-2.2.4/bin:/Users/jeevanfarias/.rvm/gems/ruby-2.2.4@global/bin:/Users/jeevanfarias/.rvm/rubies/ruby-2.2.4/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/TeX/texbin:/opt/X11/bin:/Applications/Wireshark.app/Contents/MacOS:/Users/jeevanfarias/.rvm/bin
+      export PATH=$PATH:/usr/local/lib/node_modules/npm/node_modules/npm-lifecycle/node-gyp-bin:/Users/dimitrileggas/Documents/hyfa/location_tracker/node_modules/nodejs-mobile-react-native/node_modules/.bin:/Users/dimitrileggas/Documents/hyfa/location_tracker/node_modules/.bin:/anaconda2/bin:/Users/dimitrileggas/google-cloud-sdk/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/TeX/texbin
       node $@
     
